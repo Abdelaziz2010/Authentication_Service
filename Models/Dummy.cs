2165bdcc818ed900d663e0705c6676e66a20c44b
abdelaziz <abdelazizsaleh1999@gmail.com>
2025-07-24 15:05:53 +0200

    Add Dummy class to Authentication_Service.Models
    Introduced a new `Dummy` class with properties `Id`, `Name`, and `Description`.
    Implemented a constructor for initialization and a `ToString` method for object representation.

---
+++ Models/Dummy.cs
@@ -0,0 +1,19 @@
+﻿namespace Authentication_Service.Models
+{
+    public class Dummy
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Dummy(int id, string name, string description)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+        }
+        public override string ToString()
+        {
+            return $"Dummy(Id: {Id}, Name: {Name}, Description: {Description})";
+        }
+    }
+}
